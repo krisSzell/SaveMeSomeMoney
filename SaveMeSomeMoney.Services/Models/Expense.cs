d16aa2b29b7c7ba5b57e236d55af410e5ff0adfc
Krzysztof Skorupa <skorupa.krzysztof1@gmail.com>
2018-09-11 12:31:12 +0200

    added expense categories api endpoint, ignoring foreign keys on serialization

--- SaveMeSomeMoney.Services/Models/Expense.cs
+++ SaveMeSomeMoney.Services/Models/Expense.cs
@@ -1,12 +1,15 @@
-﻿namespace SaveMeSomeMoney.Services.Models
+﻿using Newtonsoft.Json;
+
+namespace SaveMeSomeMoney.Services.Models
 {
-    public class Expense
+    public class Expense : IEntity
     {
         public int Id { get; set; }
         public string Description { get; set; }
         public decimal Value { get; set; }
-
-        public ExpenseCategory ExpenseCategory { get; set; }
         public int ExpenseCategoryId { get; set; }
+
+        [JsonIgnore]
+        public ExpenseCategory ExpenseCategory { get; set; }
     }
 }
--- SaveMeSomeMoney.Services/Repositories/Interfaces/IExpensesRepository.cs
+++ SaveMeSomeMoney.Services/Repositories/Interfaces/IExpensesRepository.cs
@@ -1,14 +1,8 @@
 ﻿using SaveMeSomeMoney.Services.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace SaveMeSomeMoney.Services.Repositories.Interfaces
 {
-    public interface IExpensesRepository
+    public interface IExpensesRepository : IRepository<Expense>
     {
-        Task<List<Expense>> GetAll();
-        Task AddAsync(Expense expense, bool shouldSaveContext = true);
-        Task<bool> UpdateAsync(int id, Expense expense);
-        Task<bool> DeleteAsync(int id);
     }
 }
